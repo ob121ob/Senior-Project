0b14047aed77b8c791d9b22945c36710d8f28d82
James OB <jo663859@sju.edu>
2020-02-17 12:23:45 -0500

    3rd attempt to connect

--- testingGitConnection/src/testingGitConnection/testingGitConnection.java
+++ testingGitConnection/src/testingGitConnection/testingGitConnection.java
@@ -4,7 +4,7 @@ public class testingGitConnection {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("HelloWorld!");
+System.out.println("HelloWorld!  ");
 	}
 
 }
